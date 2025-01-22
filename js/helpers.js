466bdabcbfeb6f85e374300fff398b19329adc03
Tobi K <kudayisitobi@gmsil.com>
2025-01-22 17:47:20 +0100

    added meta keywords

---
+++ js/helpers.js
@@ -0,0 +1,15 @@
+
+const metaKeywords = `
+creative, modern, clean, cafe, cafe as a service, blog, 
+agency, corporate, studio, branding, designer, photocopy, 
+printing, lamination, typing, scanning, online registration, 
+cv, binding, letterhead design, photo shoot, product shoot,
+corporate branding, computer training courses, graphic design courses,
+website development courses, hospitality, hotel, short rest, full lodging,
+best hotel, shortlets, apartments, luxury apartments, 69, shawarma, drinks, lounge, bar, club, apapa road, costain,
+real estate, house for rent, house for sale, land, lease, rent, estate, bayo 69,
+`
+
+const initMetaContent = () => {
+    $('#keywords-meta').attr('content',metaKeywords)
+}
