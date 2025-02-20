7c613d4ebfd2cafd3d48d4fe5291b36c524d5bc2
Tobi K <kudayisitobi@gmsil.com>
2025-02-20 10:23:37 +0100

    updates

--- js/mmm.js
+++ js/mmm.js
@@ -1,3 +1,10 @@
 $(() => {
     initMetaContent()
 })
+
+$(() => {
+    $('#caas-btn').click(e => {
+        e.preventDefault()
+        const caasName = $('#caas-name').val(), caasName = $('#caas-name').val(),
+    })
+})
